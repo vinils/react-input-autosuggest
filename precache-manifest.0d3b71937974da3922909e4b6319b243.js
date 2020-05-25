992e65c4eb94d4deb167947a4a3e6d9db755895b
Vinícius L. Santos <vinicius.lourencosantos@aveva.com>
2020-05-25 13:25:49 -0300

    Updates

---
+++ precache-manifest.0d3b71937974da3922909e4b6319b243.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dedc997e04ad5c0b1117e482a869fa80",
+    "url": "/react-input-autosuggest/index.html"
+  },
+  {
+    "revision": "61c2948bc92f4b6a7bc2",
+    "url": "/react-input-autosuggest/static/css/main.9e24c1c7.chunk.css"
+  },
+  {
+    "revision": "532f72b6278200e8b42c",
+    "url": "/react-input-autosuggest/static/js/2.45d4bbe5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-input-autosuggest/static/js/2.45d4bbe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61c2948bc92f4b6a7bc2",
+    "url": "/react-input-autosuggest/static/js/main.202a1519.chunk.js"
+  },
+  {
+    "revision": "557b1430187bdbf009f2",
+    "url": "/react-input-autosuggest/static/js/runtime-main.aba11d86.js"
+  }
+]);
